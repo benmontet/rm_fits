f4c55d94f32ee1fcf368fd9c4ccbca33249e21d4
benmontet <bmontet@uchicago.edu>
2019-09-07 15:41:14 -0500

    running mcmc

--- code/toy_mcmc.py
+++ code/toy_mcmc.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import starry
 
-import exoplanet as exo
+#import exoplanet as exo
 import emcee
 
 
@@ -26,24 +26,26 @@ tuse = time + 0.0
 euse = verr + 0.0
 vuse = vels + 0.0
 
-bnds = ((12000, 24000), (0.04, 0.07), (-1.0, 0.0), (15,25), (0,1),(0,1), (0,90), (-20,20),
-        (50,300), (0.0, 2.0), (2.0, 20.0), (0.0, 1.0), (0.12, 0.20))
+bnds = ((12000, 24000), (0.04, 0.09), (-1.0, 0.0), (15,25), (0,1),(0,1), (-70,90), (-40,20),(50,1000), (-40000, 6000),
+        (-300000, 300000), (-100000, 500000),(0.0, 3.0), (0.0, 30.0), (0.0, 1.0), (0.7, 1.0), (0.16, 0.175))
 
 
-vsini_mu = 18.3
-vsini_sig = 1.8
+vsini_mu = 18300
+vsini_sig = 1800
 
-ndim = 13
-nwalkers = 30
+ndim = 17
+nwalkers = 500
 
-init = np.array([18300.012920585246, 0.058325908653385085, -0.17989438892573809, 20.56513997921657, 1.0, 0.0, 17.311534969903935,
-        -6.657428484257285, 185.09863367853995, 1.2029170041130754, 5.885716645051851, 0.01, 0.165])
+init = np.array([19299.999257861025, 0.06078305799279333, -0.26842308404132836, 20.45127084401793, 0.6,
+                 0.2, 10.048742594672486, -12., 270., 100., -16500., 110000.,
+                 1.0844716946805428,
+                 1.0055472803019998, 0.6647339097213981, 0.8308688052159306, 0.16522744788036733])
 
 
 def rmcurve(params):
-    vsini, r, b, a, u1, u2, obl, gamma, gammadot, jitter_good, jitter_bad, q, t0 = params
-    veq = vsini / np.sin(inc * np.pi / 180.0)
 
+    vsini, r, b, a, u1, u2, obl, gamma, gammadot, gammadotdot, gamma3, gamma4, jitter_good, jitter_bad, q, factor, t0 = params
+    veq = vsini / np.sin(inc * np.pi / 180.0)
 
     map.inc = inc
     map.obl = obl
@@ -51,38 +53,40 @@ def rmcurve(params):
     map[1:] = [u1, u2]
     map.veq = veq
 
-    #orbit = exo.orbits.KeplerianOrbit(period=P, a=a, t0=t0, b=b, ecc=e, omega=w, r_star=1.0)
+    f = (tuse - t0) / P * 2 * np.pi
+    I = np.arccos(b / a)
 
-    #x, y, z = orbit.get_relative_position(tuse)
+    zo = a * np.cos(f)
+    yo = -a * np.sin(np.pi / 2 + f) * np.cos(I)
+    xo = a * np.sin(f) * np.sin(I)
 
-    #xo = x.eval()
-    #yo = y.eval()
-    #33zo = z.eval()
-    #theta = 360.0 / Prot * tuse
+    theta = 360.0 / Prot * tuse
 
-    xo = np.ones_like(tuse)
-    yo = np.ones_like(tuse)
-    zo = np.ones_like(tuse)*10.0
-    theta = np.ones_like(tuse)
-
-    rv = map.rv(xo=xo, yo=yo, zo=zo, ro=r, theta=theta)
-    rv += gamma + gammadot * (tuse - 0.15)
+    rv_0 = map.rv(xo=xo, yo=yo, zo=zo, ro=r, theta=theta)
+    trend = gamma + gammadot * (tuse - 0.15) + gammadotdot * (tuse - 0.15) ** 2 + gamma3*(tuse-0.15)**3 + gamma4*(tuse-0.15)**4
+    rv = rv_0 + trend
 
     var_good = (euse ** 2 + jitter_good ** 2)
     var_bad = (euse ** 2 + jitter_bad ** 2)
-    gooddata = -0.5 * q * (np.sum((rv - vuse) ** 2 / var_good + np.log(2 * np.pi * var_good)))
-    baddata = -0.5 * (1 - q) * (np.sum((rv - vuse) ** 2 / var_bad + np.log(2 * np.pi * var_bad)))
-    lnprob = gooddata + baddata
+
+    goodgauss = q / np.sqrt(2 * np.pi * var_good) * np.exp(-(rv - vuse) ** 2 / (2 * var_good))
+    badgauss = (1 - q) / np.sqrt(2 * np.pi * var_bad) * np.exp(-(rv_0 * factor + trend - vuse) ** 2 / (2 * var_bad))
+
+    lnprob = np.log(goodgauss + badgauss)
 
     #print(-1 * lnprob)
-    return lnprob
+    return np.sum(lnprob)
 
 def set_priors(params):
-    vsini, r, b, a, u1, u2, obl, gamma, gammadot, jitter_good, jitter_bad, q, t0 = params
+    vsini, r, b, a, u1, u2, obl, gamma, gammadot, gammadotdot, gamma3, gamma4, jitter_good, jitter_bad, q, factor, t0 = params
     lnprior = 0
 
     if not all(b[0] < v < b[1] for v, b in zip(params, bnds)):
         return -np.inf
+
+    if u1 + u2 > 1.0:
+        return -np.inf
+
     lnprior -= 0.5*(vsini - vsini_mu)**2/vsini_sig**2
 
     return lnprior
@@ -95,6 +99,7 @@ def lnprob(params):
         return -np.inf
     else:
         lnlike = rmcurve(params)
+        #print(lnlike, lnprior)
         return lnlike + lnprior
 
 def setup_data(params):
@@ -104,11 +109,18 @@ def setup_data(params):
 
     varystd = np.ones(ndim)*0.2
     varystd[0] = 100.0
-    varystd[1] = 0.01
-    varystd[12] = 0.01
-    varystd[7] = 3.0
-    varystd[8] = 3.0
-    varystd[11] = 0.001
+    varystd[1] = 0.005
+    varystd[2] = 0.05
+    varystd[4] = 0.02
+    varystd[5] = 0.02
+    varystd[14] = 0.01
+    varystd[7] = 2.0
+    varystd[8] = 10.0
+    varystd[9] = 30.0
+    varystd[10] = 200.0
+    varystd[11] = 2000.0
+    varystd[15] = 0.01
+    varystd[16] = 0.001
 
     for i in range(len(varystd)):
         p0[:,i] = p0[:,i] + np.random.normal(0, varystd[i], nwalkers)
@@ -117,11 +129,16 @@ def setup_data(params):
 
 p0 = setup_data(init)
 
-sampler = emcee.EnsembleSampler(nwalkers, ndim, lnprob)
-pos, prob, state = sampler.run_mcmc(p0, 2000, progress=True)
+with Pool(24) as pool:
+    sampler = emcee.EnsembleSampler(nwalkers, ndim, lnprob, pool=pool)
+    pos, prob, state = sampler.run_mcmc(p0, 3000, progress=True)
+
 
 best = np.where(sampler.flatlnprobability == np.max(sampler.flatlnprobability))[0][0]
 print(sampler.flatlnprobability[best])
 
+np.save('chain', sampler.chain)
+np.save('pos', pos)
+np.save('prob', prob)
 
 
